0babcb49d7a3c7e3d1b1cf93b584b9dc693a2927
abhishekanand22 <m.abhishekanand9@gmail.com>
2024-06-01 14:45:12 +0530

    To-Do List - HTML , CSS , JS

---
+++ script.js
@@ -0,0 +1,36 @@
+function addTask() 
+{
+    let input = document.getElementById("taskInput");
+    let taskText = input.value.trim();
+    let taskList = document.getElementById("taskList");
+    let taskCount = taskList.getElementsByTagName("li").length;
+
+    if (taskCount >= 5) 
+    {
+        alert("Too Many Tasks Added!");
+        return; 
+    }
+
+    if (taskText !== "") 
+    {
+        let li = document.createElement("li");
+        li.textContent = taskText;
+        
+        
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "&times;"; 
+        deleteBtn.className = "delete-btn";
+        deleteBtn.onclick = function() 
+        {
+            li.remove(); 
+        };
+
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+        input.value = "";
+    } 
+    else
+    {
+        alert("Please enter a task!");
+    }
+}
